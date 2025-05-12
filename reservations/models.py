6517adb4c1e91687bcd116ffd26e84983f0213bc
Roullito <jmoleins@laposte.net>
2025-05-12 18:41:55 +0200

    feat: model client

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    first_name = models.CharField(max_length=50)  # Prénom
+    last_name = models.CharField(max_length=50)   # Nom
+    email = models.EmailField(unique=True)        # Email (doit être unique)
+    phone = models.CharField(max_length=20)       # Numéro de téléphone
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
